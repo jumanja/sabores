df13f80ef2268d75921d856e1c97e10f507a5d52
jumanja <jumanja@gmail.com>
2018-10-30 21:49:47 -0500

    Archivos iniciales para el Foro

---
+++ HolaMundo.php
@@ -0,0 +1,12 @@
+<?php
+echo "<h1>Hola Mundo de Sabores!</h1>";
+echo "<h2>Integrantes</h2>";
+echo "<ul>";
+echo "<li>Manjarrés Pabón Juan Luis</li>";
+echo "<li>Pardo Rueda Kimberly Johana</li>";
+echo "<li>Pineda Otero Néstor Antonio</li>";
+echo "</ul>";
+echo "PHP version: " . PHP_VERSION;
+echo "<br>"
+echo "github: https://github.com/jumanja/sabores.git";
+?>
--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Sabores proyecto para IC
 
 Este proyecto se crea como proyecto de aula para Integración Contínua.
+
+## Integrantes
+Manjarrés Juan
+Pardo Kimberly
+Pineda Néstor
+
+2018
