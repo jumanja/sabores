1ecde688bf48dd3ba461370e8b0b22905a0e94e2
kimpardor <44640977+kimpardor@users.noreply.github.com>
2018-11-04 04:06:04 -0500

    Add files via upload

---
+++ holamundo/HolaMundo.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Sabores para IC</title>
+        
+    </head>
+    <body>
+
+    <body style="background-color: darkcyan">
+
+        <center>
+        <h1> Hola Mundo de Sabores! </h1>
+        <h2> Integrantes </h2>     
+        <li> Manjarrés Pabón Juan Luis </li>
+        <li> Pardo Rueda Kimberly Johana </li>
+        <li> Pineda Otero Néstor Antonio </li>
+        
+        <br>
+        <br>
+
+        versión PHP: "  .  PHP_VERSION 
+        <br>
+        github: https://github.com/jumanja/sabores.git
+        <br>
+        <br>
+        <img src="CocinaFacil.jpg" alt=""/>
+        </center>
+    </body>
+</html>
