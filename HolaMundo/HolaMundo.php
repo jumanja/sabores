58177413b7989b44d7a34a8af8c6dbb49db17344
kimpardor <44640977+kimpardor@users.noreply.github.com>
2018-11-04 04:15:15 -0500

    Update HolaMundo.php

--- HolaMundo/HolaMundo.php
+++ HolaMundo/HolaMundo.php
@@ -1,17 +1,29 @@
-<?php
-echo "<html><head><title>Sabores para IC</title>";
-echo '<meta charset="UTF-8"';
-echo "<body>";
-echo "<h1>Hola Mundo de Sabores!</h1>";
-echo "<h2>Integrantes</h2>";
-echo "<ul>";
-echo "<li>Manjarrés Pabón Juan Luis</li>";
-echo "<li>Pardo Rueda Kimberly Johana</li>";
-echo "<li>Pineda Otero Néstor Antonio</li>";
-echo "</ul>";
-echo "PHP version: " . PHP_VERSION;
-echo "<br>";
-echo "github: https://github.com/jumanja/sabores.git";
-echo "</body>";
-echo "</html>";
-?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Sabores para IC</title>
+        
+    </head>
+    <body>
+
+    <body style="background-color: darkcyan">
+
+        <center>
+        <h1> Hola Mundo de Sabores! </h1>
+        <h2> Integrantes </h2>     
+        <li> Manjarrés Pabón Juan Luis </li>
+        <li> Pardo Rueda Kimberly Johana </li>
+        <li> Pineda Otero Néstor Antonio </li>
+        
+        <br>
+        <br>
+
+        versión PHP: "  .  PHP_VERSION 
+        <br>
+        github: https://github.com/jumanja/sabores.git
+        <br>
+        <br>
+        <img src="CocinaFacil.jpg" alt=""/>
+        </center>
+    </body>
+</html>
